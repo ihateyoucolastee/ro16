36429e139a398d083c8b3877921ab07927608ce6
colastee <141801820+repoTestn@users.noreply.github.com>
2024-06-20 14:44:22 -0700

    Create OTHERTESTING.lua

---
+++ OTHERTESTING.lua
@@ -0,0 +1,2 @@
+local rs = loadstring(game:HttpGetAsync("https://github.com/richie0866/rbxm-suite/releases/latest/download/rbxm-suite.lua"))()
+rs.launch(rs.download("repoTestn/Roblox16Feel@Ro16Feel", "V1.rbxm")) --or other versions
